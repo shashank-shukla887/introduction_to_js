80d5f26b833cb72304e38fe352a44197c8e8542b
Shashank <SHASHANKSHUKLA858@GMAIL.COM>
2023-04-22 16:17:58 +0530

    day1 : intro of js ,some basic commands,array,map reduce filter

---
+++ js_fucntions.js
@@ -0,0 +1,61 @@
+// let val='test';
+
+// function test1()
+// {
+//     let val='test1';
+//     console.log(val);
+// }
+
+// console.log(val);
+// test1();
+// console.log(val);
+// test1();
+
+let a=5;
+let b=10;
+let total=0;
+
+// function add(val1,val2)
+// {
+//     val2=val2==undefined?0:val2;
+//     console.log(val2);
+//     total+=(val1*val2);
+//     console.log(total);
+// }
+
+// add(a);
+// add(50);
+
+
+// function add(val1,val2)
+// {    val2=val2||1;
+//     // val2=val2==undefined?0:val2;
+//     console.log(val2);
+//     total+=(val1*val2);
+//     console.log(total);
+// }
+
+// add(a);
+// add(50);
+
+// function add(a,b)
+// {
+//     console.log(a,b);
+//     let total=a*b;
+//     return total;
+
+
+// }
+
+// console.log(add(5,5));
+
+var test1=function(x)
+{
+    return x*5;
+}
+
+const test2=(x)=>x*5;
+console.log(test1(10));
+console.log(test2(10));
+
+
---
+++ tempCodeRunnerFile.js
@@ -0,0 +1,10 @@
+function add(a,b)
+// {
+//     console.log(a,b);
+//     let total=a*b;
+//     return total;
+
+
+// }
+
+// console.log(add(5,5));
